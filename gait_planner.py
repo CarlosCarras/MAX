ae8bdfaf73a72561c8a3abf21d4922b3fb7ad4b3
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2020-11-29 16:53:18 -0500

    stand test

--- gait_planner.py
+++ gait_planner.py
@@ -1,18 +1,19 @@
+from __future__ import division
 import time
 from threading import Timer
 
 
 GAIT_TROT = 0x00
-STEP_RIGHT = "R"
-STEP_LEFT = "L"
-STEP_UP = "U"
-STEP_DOWN = "D"
+STEP_FORWARD_RIGHT = 0
+STEP_FORWARD_LEFT = 1
 
+LEG_FR = 0
+LEG_FL = 1
+LEG_RR = 2
+LEG_RL = 3
 
-TROT_STEP_RIGHT_POSE = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-TROT_STEP_LEFT_POSE = [0, 0, 0, -20, 20, 0, 0, 0, 0, 0, 0, 0]
-TROT_STEP_LEFT_SWING = [0, 0, 0, 0, -20, 0, 0, 0, 0, 0, 0, 0]
-TROT_STEP_RIGHT_SWING = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+LIFT_LEGS = [0, 0, 0, -20, 20, 0, 0, 0, 0, 0, 0, 0]
+SWING_LEGS = [0, 0, 0, 0, -20, 0, 0, 0, 0, 0, 0, 0]
 
 BOW = [0, 0, 0, -20, 20, 0, 0, 0, 0, 0, 0, 0]
 BODY_ROLL = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -35,40 +36,54 @@ class GaitPlanner():
             self.gait = GAIT_TROT
 
 
-    def step_trot(self, step_dir):
-        if self.last_step == STEP_RIGHT:
-            change = TROT_STEP_LEFT_POSE
-            swing = TROT_STEP_LEFT_SWING
-        else:
-            change = TROT_STEP_RIGHT_POSE
-            swing = TROT_STEP_RIGHT_SWING
-
-        if step_dir == STEP_DOWN:
-            change =  [i*-1 for i in change]
+    def update_legs(self, change, legs):
+        for i in range(len(change)):
+            if not i//3 in legs:
+                change[i] = 0
 
         self.controller.change_pose(change)
         self.controller.update()
-        self.step_time = time.time()
-        time.sleep(0.2)
 
-        self.controller.change_pose(swing)
-        self.controller.update()
+
+    def raise_legs(self, legs):
+        self.update_legs(LIFT_LEGS, legs)
+
+
+    def swing_legs(self, legs):
+        self.update_legs(SWING_LEGS, legs)
+
+
+    def lower_legs(self, legs):
+        change = [LIFT_LEGS[i]*-1 for i in LIFT_LEGS]
+        self.update_legs(change, legs)
+        self.step_time = time.time()
+
+
+    def restore_legs(self, legs):
+        change = [SWING_LEGS[i] * -1 for i in SWING_LEGS]
+        self.update_legs(change, legs)
 
 
     def step(self):
         if time.time() - self.step_time < self.wait_time:
             return
-
-        if self.last_step == STEP_RIGHT:
-            self.last_step = STEP_LEFT
-        else:
-            self.last_step = STEP_RIGHT
+        self.last_step = not self.last_step
 
         if self.gait is GAIT_TROT:
-            self.step_trot(STEP_UP)
+            if self.last_step == STEP_FORWARD_RIGHT:
+                legs = [LEG_FL, LEG_RR]
+            else:
+                legs = [LEG_FR, LEG_RL]
+        else:
+            legs = []
 
-            step_down = Timer(0.5, self.step_trot, STEP_DOWN)
-            step_down.start()
+        self.raise_legs(legs)
+        self.swing_legs(legs)
+        self.lower_legs(legs)
+        self.restore_legs(legs)
+        #Timer(0.5, self.lower_legs, legs).start()
+
+
 
 
 
