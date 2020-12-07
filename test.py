e9e2f82235af244376ea8ed6e86fbba034db6fc9
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2020-12-07 03:31:22 -0500

    testing imu

--- test.py
+++ test.py
@@ -292,14 +292,12 @@ if __name__ == "__main__":
     imu = ICM20948()
 
     while True:
-        x, y, z = imu.read_magnetometer_data()
         ax, ay, az, gx, gy, gz = imu.read_accelerometer_gyro_data()
 
         print("""
 Accel: {:05.2f} {:05.2f} {:05.2f}
-Gyro:  {:05.2f} {:05.2f} {:05.2f}
-Mag:   {:05.2f} {:05.2f} {:05.2f}""".format(
-            ax, ay, az, gx, gy, gz, x, y, z
+Gyro:  {:05.2f} {:05.2f} {:05.2f}""".format(
+            ax, ay, az, gx, gy, gz
         ))
 
-        time.sleep(0.25)
+        time.sleep(0.5)
