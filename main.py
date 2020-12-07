1bbbb59f573e4f7cb849c53781be54f6cca8fcec
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2020-12-07 14:58:57 -0500

    testing imu

--- main.py
+++ main.py
@@ -33,17 +33,8 @@ def main():
     time.sleep(5)
     move.rest()
 
-    #input('Press [Enter] to start walking.')
 
-    #move.walk_forward(30)
-    #health.status()
-
-    #input('Press [Enter] to test Xbox.')
-    #xbox.test(30)
-    #health.status()
-
-    input('Press [Enter] to test Thermal.')
-    perception.track_hotspot(30)
+    move.walk_forward(30)
 
 
 
